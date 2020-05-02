1ad5a048079280744af23d5e54e3e22b6c6c6e54
Chaitra-bhat <34712573+Chaitra-bhat@users.noreply.github.com>
2020-05-02 11:00:52 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -2,3 +2,23 @@
 friend_age = {"Rolf":24,
               "Adam":25,
               "Charlie":30}
+print(friend_age["Rolf"])
+print("\n")
+
+
+#change existing key
+
+friend_age["Rolf"] = 25
+print(friend_age)
+
+riends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+
+# You can turn a list of lists or tuples into a dictionary:
+
+friends = [("Rolf", 24), ("Adam", 30), ("Anne", 27)]
+friend_ages = dict(friends)
+print(friend_ages)
